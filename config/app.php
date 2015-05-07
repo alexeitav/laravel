c16b82ab8e7fb194369d758cccf24f62647515c1
Alexei C Tavares <alexei_tav@hotmail.com>
2015-05-07 11:23:38 -0300

    facede form

--- config/app.php
+++ config/app.php
@@ -199,6 +199,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+        'Form'      => 'Illuminate\Html\FormFacade',
+        'Html'      => 'Illuminate\Html\HtmlFacade',
 
 	],
 
